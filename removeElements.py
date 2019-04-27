4a9a601b1ed1aadd0df5433af50523ed29efcce2
stephenc <stephenchenxj@gmail.com>
2019-04-27 14:19:41 -0700

    bug fixed

--- removeElements.py
+++ removeElements.py
@@ -7,10 +7,10 @@ Created on Sat Apr 27 13:41:00 2019
 """
 
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution(object):
     def removeElements(self, head, val):
@@ -26,9 +26,34 @@ class Solution(object):
             if cur.val == val: # need to delete cur
                 if pre: # got pre node
                     pre.next = cur.next
+                    node2del = cur
+                    cur = cur.next
+                    del node2del
                 else: 
                     head = cur.next
-            pre = cur
-            cur = cur.next
+                    node2del = cur
+                    cur = cur.next
+                    del node2del
+            else: # keep cur                
+                pre = cur
+                cur = cur.next
             
         return head
+    
+    
+def main():
+    nodeA = ListNode(1)    
+    nodeB = ListNode(1)
+    nodeA.next = nodeB
+    
+    mySolution = Solution()
+    result = mySolution.removeElements(nodeA, 1)
+    
+    while result:
+        print(result.val)
+        result = result.next
+    
+    
+if __name__ == '__main__':
+    main()
+    
