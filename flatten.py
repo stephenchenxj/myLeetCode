ad128b47ee34a5876fc167b12e7d08c8a20079e3
stephenc <stephenchenxj@gmail.com>
2019-05-13 21:48:51 -0700

    fixed one bug: clear all child in items of the flatten list

--- flatten.py
+++ flatten.py
@@ -33,6 +33,7 @@ class Solution(object):
                     #print(currentNode.next.val)
                 child = currentNode.child
                 currentNode.next = child
+                currentNode.child = None
                 child.prev = currentNode
                 currentNode = child
                 
@@ -72,7 +73,11 @@ def main():
     node7 = Node(7)
     node8 = Node(8)  
     node9 = Node(9)
+    node10 = Node(10)
+    node11 = Node(11)  
+    node12 = Node(12)
     
+    '''
     node1.next = node2
     node2.prev = node1
     node2.next = node3
@@ -83,6 +88,37 @@ def main():
     node5.prev = node4
     
     node5.child = node6
+    
+    
+    flatten = Solution().flatten(node1)
+    
+    while flatten:
+        print (flatten.val)
+        flatten = flatten.next
+    '''    
+    
+    node1.next = node2
+    node2.prev = node1
+    node2.next = node3
+    node3.prev = node2    
+    node3.next = node4
+    node4.prev = node3
+    node4.next = node5
+    node5.prev = node4
+    node5.next = node6
+    node6.prev = node5
+    
+    node3.child = node7
+    node7.next = node8
+    node8.prev = node7
+    node8.next = node9
+    node9.prev = node8
+    node9.next = node10
+    node10.prev = node9
+    
+    node8.child = node11
+    node11.next = node12
+    node12.prev = node11
     
     
     flatten = Solution().flatten(node1)
