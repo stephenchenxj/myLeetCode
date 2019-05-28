60552e37a4d23e7d8a9b575afaa5cfafdafd08c6
Stephen Chen <stephen.chen@finisar.com>
2019-05-28 15:07:30 -0700

    still have bugs

--- openLock.py
+++ openLock.py
@@ -16,16 +16,18 @@ class Solution(object):
         steps = 0
         deadNchecked = set(deadends)
         print (deadNchecked)
-        move = {'0':['9', '1'], '1':['0', '2'], '2':['1', '3'], '3':['2', '4'],
-                '4':['3', '5'], '5':['4', '6'], '6':['5', '7'], '7':['6', '8'], 
-                '8':['7', '9'], '9':['8', '0']}
+#        move = {'0':['9', '1'], '1':['0', '2'], '2':['1', '3'], '3':['2', '4'],
+#                '4':['3', '5'], '5':['4', '6'], '6':['5', '7'], '7':['6', '8'], 
+#                '8':['7', '9'], '9':['8', '0']}
+        move = {}
+        for i in range(10):
+            move[str(i)] = [str(((i-1)+10)%10), str((i+1)%10)]
+    
         q = ['0000']
         while q: # when q is not empty
             steps += 1
+            newQ = []
             for key in q:
-                if key == target:
-                    return steps
-                
                 for i in range(4):
                     newKey1 = key[:i] + move[key[i]][0] + key[i+1:]
                     newKey2 = key[:i] + move[key[i]][1] + key[i+1:]
@@ -33,17 +35,26 @@ class Solution(object):
                     #print(newKey2)
                     if newKey1 in deadNchecked:
                         continue
+                    elif newKey1 == target:
+                        return steps
                     else:
-                        q.append(newKey1)
-                        deadNchecked.add(newKey1)
+                        newQ.append(newKey1)
+                        #deadNchecked.add(newKey1)
                         
                     if newKey2 in deadNchecked:
                         continue
+                    elif newKey2 == target:
+                        return steps
                     else:
-                        q.append(newKey2)
-                        deadNchecked.add(newKey2)
-                deadNchecked.add(key)
-                q.remove(key)
+                        newQ.append(newKey2)
+                        #deadNchecked.add(newKey2)
+#                print( 'steps = %d , newQ is ' %steps)
+#                print (newQ)
+                        
+                #deadNchecked.add(key)
+                
+            
+            q = newQ
         
         return -1
         
@@ -52,9 +63,13 @@ class Solution(object):
         
         
 def main():
-    deadends = ['1234', '2000']
-    target = '3210'
+    deadends = ["0100"]
+    target = '2200'
     print(Solution().openLock(deadends,target))
+    
+#    deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"]
+#    target = '8888'
+#    print(Solution().openLock(deadends,target))
     
 if __name__ == '__main__':
     main()
