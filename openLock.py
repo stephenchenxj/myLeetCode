a2e4dbf00dfee00e765f90bb84d8656c029d3a14
Stephen Chen <stephen.chen@finisar.com>
2019-05-29 11:25:58 -0700

    accepted

--- openLock.py
+++ openLock.py
@@ -19,17 +19,21 @@ class Solution(object):
         for i in range(10):
             move[str(i)] = [str(((i-1)+10)%10), str((i+1)%10)]
     
-        q = ['0000']
+        startingPoint = '0000'        
+        if startingPoint in deadNchecked:
+            return -1
+        q = {startingPoint}
+        
         while q: # when q is not empty
             steps += 1
-            newQ = []
+            newQ = set() # have to use set, not list. can't add repeated items
             for key in q:
                 for i in range(8):
                     newKey = key[:int(i/2)] + move[key[int(i/2)]][i%2] + key[int(i/2)+1:]
                     if newKey == target:
                         return steps
                     elif newKey not in deadNchecked:
-                        newQ.append(newKey)
+                        newQ.add(newKey)
             for key in q:
                 deadNchecked.add(key)
             q = newQ
@@ -41,13 +45,13 @@ class Solution(object):
         
         
 def main():
-    deadends = ["0500"]
-    target = '3250'
+    deadends = ["0001"]
+    target = '8888'
     print(Solution().openLock(deadends,target))
     
-#    deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"]
-#    target = '8888'
-#    print(Solution().openLock(deadends,target))
+    deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"]
+    target = '8888'
+    print(Solution().openLock(deadends,target))
     
 if __name__ == '__main__':
     main()
