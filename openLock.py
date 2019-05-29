a1dd7a8e36284649ce6c572125aecca08caacdbd
Stephen Chen <stephen.chen@finisar.com>
2019-05-29 11:13:03 -0700

    memory error

--- openLock.py
+++ openLock.py
@@ -15,10 +15,6 @@ class Solution(object):
         """
         steps = 0
         deadNchecked = set(deadends)
-        print (deadNchecked)
-#        move = {'0':['9', '1'], '1':['0', '2'], '2':['1', '3'], '3':['2', '4'],
-#                '4':['3', '5'], '5':['4', '6'], '6':['5', '7'], '7':['6', '8'], 
-#                '8':['7', '9'], '9':['8', '0']}
         move = {}
         for i in range(10):
             move[str(i)] = [str(((i-1)+10)%10), str((i+1)%10)]
@@ -34,19 +30,9 @@ class Solution(object):
                         return steps
                     elif newKey not in deadNchecked:
                         newQ.append(newKey)
-                        #deadNchecked.add(newKey1)
-                        
-#                print( 'steps = %d , newQ is ' %steps)
-#                print (newQ)
-                        
-                #deadNchecked.add(key)
-                
+            for key in q:
                 deadNchecked.add(key)
             q = newQ
-#            print( 'steps = %d , newQ is ' %steps)
-#            print (newQ)
-#            print( ' deadNchecked is ')
-#            print (deadNchecked)
         
         return -1
         
@@ -56,12 +42,12 @@ class Solution(object):
         
 def main():
     deadends = ["0500"]
-    target = '3000'
+    target = '3250'
     print(Solution().openLock(deadends,target))
     
-    deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"]
-    target = '8888'
-    print(Solution().openLock(deadends,target))
+#    deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"]
+#    target = '8888'
+#    print(Solution().openLock(deadends,target))
     
 if __name__ == '__main__':
     main()
