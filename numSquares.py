d38c301177b2a54cb8f7949de36bfd37f9b88b3f
Stephen Chen <stephen.chen@finisar.com>
2019-05-30 18:58:57 -0700

    accepted. but need more work

--- numSquares.py
+++ numSquares.py
@@ -9,6 +9,7 @@ Created on Wed May 29 20:45:50 2019
 import math
 
 class Solution(object):
+    tempResult = {}
     def numSquares(self, n):
         """
         :type n: int
@@ -17,13 +18,31 @@ class Solution(object):
         maxNum = int(math.sqrt(n))
         
         #print(maxNum)
-        node =[]
+        nodes =[]
         for i in range(maxNum):
-            node.append ( (maxNum - i)**2 )
-        #print(node)
+            nodes.append ( (maxNum - i)**2 )
+        #print(nodes)
+        
+        num = 0
+        minCount = n
+        
+        
+        i = 0
+        for node in nodes:
+            i += 1
+            if (n-node) not in self.tempResult.keys():
+                self.tempResult[n-node] =  self.numSquares(n - node)
+            num = 1 + self.tempResult[n-node]
+            if minCount > num:
+                minCount = num
+                
+            if i > 20:
+                break
+        
+        return minCount
         
 def main():
-    print(Solution().numSquares(17))
+    print(Solution().numSquares(7168))
     
 if __name__ == '__main__':
     main()
