a68c67299dc53e3c4bb3ec5f480b0c1a57496272
stephenc <stephenchenxj@gmail.com>
2019-06-02 15:42:48 -0700

    changed to BFS method

--- numSquares.py
+++ numSquares.py
@@ -15,34 +15,44 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        maxNum = int(math.sqrt(n))
         
-        #print(maxNum)
-        nodes =[]
-        for i in range(maxNum):
-            nodes.append ( (maxNum - i)**2 )
+        #create a list of squares, from 1 to int(sqrt(n))
+        squares =[]
+        for i in range(1,n):
+            square = i**2
+            if square > n:
+                break
+            squares.append ( square )
         #print(nodes)
         
         num = 0
-        minCount = n
+        
+        #initialize the toCheck set (use set, not list, because don't want to check repeated values)
+        toCheck = {n}
+        
+        while toCheck:
+            
+            nextLayer = set() #create a set for the next layer value to toCheck
+            num += 1 #num +1 for each layer
+            
+            for value in toCheck: #traverse all the values in the toCheck set
+                if value in squares: # if the value is a square itself, return num
+                    return num
+                for square in squares: #traverse all the squares in the list
+                    if value < square: # value can't consist this square, skip all bigger squares
+                        break
+                    nextLayer.add(value - square)#current value consists value and square. So the next layer value will be (value - square)
+            
+            toCheck = nextLayer # update toCheck set
+            
+        return num
         
         
-        i = 0
-        for node in nodes:
-            i += 1
-            if (n-node) not in self.tempResult.keys():
-                self.tempResult[n-node] =  self.numSquares(n - node)
-            num = 1 + self.tempResult[n-node]
-            if minCount > num:
-                minCount = num
-                
-            if i > 20:
-                break
         
-        return minCount
+
         
 def main():
-    print(Solution().numSquares(7168))
+    print(Solution().numSquares(120))
     
 if __name__ == '__main__':
     main()
