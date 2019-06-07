07537a4b5513db2e18073d6eb8adba276d8a12d2
stephenc <stephenchenxj@gmail.com>
2019-06-06 21:08:33 -0700

    accepted

--- dailyTemperatures.py
+++ dailyTemperatures.py
@@ -15,20 +15,24 @@ class Solution(object):
         
         num = len(T)
         days = [0]*num
-        biggersIndex = []
+        biggersIndex = [] # only push the local temperature peaks into the stack. 
+        #the highest temperature's index should be at the bottomn. pop peaks if they are surpassed by newer temperature peaks
         days[num-1] = 0
             
         for i in range(1, num):
-            print(T[num-i-1])
+            #print(T[num-i-1])
+            a = T[num-i-1]
             if T[num-i-1] < T[num-i]:
                 days[num-i-1] = 1
-                biggersIndex.append(num-i)
+                biggersIndex.append(num-i) #push day after today index into stack: the temperature of current day is lower than the next day
             else:
                 while biggersIndex:
-                    higherTempIndex = biggersIndex.pop()
+                    higherTempIndex = biggersIndex[-1] #get the top value in the stack, without removing it.
                     if T[num-i-1] < T[higherTempIndex]:
-                        days[num-i-1] =  higherTempIndex - (num-i)
+                        days[num-i-1] =  higherTempIndex - (num-i-1)
                         break
+                    biggersIndex.pop() #remove the index (the future day) on the top, as the temperature of the current day is higher than (or equal to) the future day. 
+                    # No need to keep that future day in the rest of the comparason. 
             
             
             
