d1e03bc6e15d2483a3138b04af49aeead7e42737
Stephen Chen <stephen.chen@finisar.com>
2019-11-17 06:40:18 -0800

    add getImportance.py

---
+++ getImportance.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 17 04:26:16 2019
+
+@author: stephen.chen
+690. Employee Importance
+Easy
+
+You are given a data structure of employee information, which includes the employee's unique id, his importance value and his direct subordinates' id.
+
+For example, employee 1 is the leader of employee 2, and employee 2 is the leader of employee 3. They have importance value 15, 10 and 5, respectively. Then employee 1 has a data structure like [1, 15, [2]], and employee 2 has [2, 10, [3]], and employee 3 has [3, 5, []]. Note that although employee 3 is also a subordinate of employee 1, the relationship is not direct.
+
+Now given the employee information of a company, and an employee id, you need to return the total importance value of this employee and all his subordinates.
+
+Example 1:
+
+Input: [[1, 5, [2, 3]], [2, 3, []], [3, 3, []]], 1
+Output: 11
+Explanation:
+Employee 1 has importance value 5, and he has two direct subordinates: employee 2 and employee 3. They both have importance value 3. So the total importance value of employee 1 is 5 + 3 + 3 = 11.
+
+ 
+
+Note:
+
+    One employee has at most one direct leader and may have several subordinates.
+    The maximum number of employees won't exceed 2000.
+
+"""
+
+# Employee info
+class Employee(object):
+    def __init__(self, id, importance, subordinates):
+        # It's the unique id of each node.
+        # unique id of this employee
+        self.id = id
+        # the importance value of this employee
+        self.importance = importance
+        # the id of direct subordinates
+        self.subordinates = subordinates
+
+class Solution(object):
+    def getImportance(self, employees, id):
+        """
+        :type employees: Employee
+        :type id: int
+        :rtype: int
+        """
+#        emap = {e.id: e for e in employees}
+#        def dfs(eid):
+#            employee = emap[eid]
+#            return (employee.importance +
+#                    sum(dfs(eid) for eid in employee.subordinates))
+#        return dfs(id)
+        
+        im = 0
+        emDict = dict()        
+        for em in employees:
+            emDict[em.id] = em
+        
+        ids = [id]
+        while ids:
+            currentId = ids.pop()
+            im += emDict[currentId].importance
+            ids.extend(emDict[currentId].subordinates)
+        
+        
+        return im
+        
+        
+        
+        
+        
+
+#[[1,5,[2,3]],[2,3,[]],[3,3,[]]]
+e2 = Employee(2,3,[])
+e3 = Employee(3,3,[])
+e1 = Employee(1, 5, [2,3])   
+inpt = [e1, e2, e3]
+id = 1
+
+#[[1,2,[2]], [2,3,[]]] , 2
+e2 = Employee(2,3,[])
+e1 = Employee(1, 2, [2])   
+inpt = [e1, e2]
+id = 2
+
+test = Solution()
+print(test.getImportance(inpt, id))
+        
+
