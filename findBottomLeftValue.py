ba4422e1d63b074ed23e19fdc536b177faeb6c54
Stephen Chen <stephen.chen@finisar.com>
2019-11-21 05:10:46 -0800

    update.

--- findBottomLeftValue.py
+++ findBottomLeftValue.py
@@ -50,5 +50,25 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
+        ans = None
+        currentLevel = [root]
+        nextLevel = []
+        
+        while currentLevel:
+            node = currentLevel.pop(0)
+            if ans == None:
+                ans = node.val
+            if node.left:
+                nextLevel.append(node.left)
+            if node.right:
+                nextLevel.append(node.right)
+            if not currentLevel:
+                if not nextLevel:
+                    return ans
+                currentLevel = nextLevel[:]
+                nextLevel = []
+                ans = None
+                
+        
         
 
