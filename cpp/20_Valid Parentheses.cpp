9e5381d912f6bbf6cfd1e876bb4d13c7e261ab59
xujian chen <stephenchenKL@gmail.com>
2020-05-20 10:55:58 -0700

    update

--- cpp/20_Valid Parentheses.cpp
+++ cpp/20_Valid Parentheses.cpp
@@ -6,4 +6,26 @@
 //  Copyright © 2020 Xujian CHEN. All rights reserved.
 //
 
-#include <stdio.h>
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> brackets;
+        set<char> left= {'(', '[', '{'};
+        unordered_map<char, char> m = {
+        {'(', ')'},
+        {'[', ']'},
+        {'{', '}'},
+    };
+        for(char c : s)
+            if (m.find(c) != m.cend()){
+                brackets.push(c);
+                
+            }else{
+                if (brackets.size() == 0) return false;
+                char l = brackets.top();
+                brackets.pop();
+                if(c != m[l]) return false;
+            }
+        return (brackets.size() == 0);
+    }
+};
